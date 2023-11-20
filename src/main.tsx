599d588e2c062d92491f9685549f7d63c81ecc9f
hariyebk <harunbekri6@gmail.com>
2023-11-20 19:02:08 +0300

    fixed the query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,25 @@ import ReactDom from "react-dom/client"
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
-import QueryProvider from "./lib/react-query/queryProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    // configuring the default options for react-query
+    defaultOptions: {
+        queries: {
+            staleTime: 0,
+        }
+    }
+})
 
 ReactDom.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-        <QueryProvider>
+        <QueryClientProvider client={queryClient}>
             <AuthProvider>
                 <App />
             </AuthProvider>
-        </QueryProvider>
+        </QueryClientProvider>
         </BrowserRouter>
     </React.StrictMode>
 )
