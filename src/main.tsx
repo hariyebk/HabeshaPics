c95fc537e6338fcd7ce34c9b84dcc0dfb24ed810
hariyebk <harunbekri6@gmail.com>
2023-11-20 20:00:04 +0300

    fixed a small bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,25 +3,16 @@ import ReactDom from "react-dom/client"
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-    // configuring the default options for react-query
-    defaultOptions: {
-        queries: {
-            staleTime: 0,
-        }
-    }
-})
+import QueryProvider from "@/lib/react-query/QueryProvider";
 
 ReactDom.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
+        <QueryProvider>
             <AuthProvider>
                 <App />
             </AuthProvider>
-        </QueryClientProvider>
+        </QueryProvider>
         </BrowserRouter>
     </React.StrictMode>
 )
