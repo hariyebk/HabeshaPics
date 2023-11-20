9ca5a180696730c72ccf29d016897bcb9e30d7b1
hariyebk <harunbekri6@gmail.com>
2023-11-20 20:34:49 +0300

    intergrated web-sdk of appwrite for CORS policy

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
 import QueryProvider from "@/lib/react-query/QueryProvider";
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+// web sdk to point the production server to appwrite
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('653badab17aa7585cd49');
 
 ReactDom.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
